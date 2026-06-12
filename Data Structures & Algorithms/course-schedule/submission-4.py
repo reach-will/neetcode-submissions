feea57a451f986f12a1494d3ae8986fd7fd2e6a5
William R. M. <101368213+reach-will@users.noreply.github.com>
2026-06-12 07:43:17 -0300

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dfs(self, node):
+        if node in self.visiting:
+            return False
+        self.visiting.add(node)
+        for c in self.course_adj_list[node]:
+            if not self.dfs(c):
+                return False
+        self.visiting.remove(node)
+        return True
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        self.course_adj_list = defaultdict(list)
+        for p in prerequisites:
+            self.course_adj_list[p[0]].append(p[1])
+        self.visiting = set()
+        for i in range(numCourses):
+            if not self.dfs(i):
+                return False
+        return True
