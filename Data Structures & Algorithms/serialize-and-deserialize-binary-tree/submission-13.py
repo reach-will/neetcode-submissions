4560c952616ed4e0ecb6b1e06d0b9ecf328ef30f
William R. M. <101368213+reach-will@users.noreply.github.com>
2026-08-04 02:00:47 -0300

    Add: serialize-and-deserialize-binary-tree - submission-13

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-13.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ""
+        result = str(root.val)
+        queue = deque([root.left, root.right])
+        while queue:
+            node = queue.popleft()
+            if not node:
+                result += ","
+                continue
+            result += "," + str(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        return result
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if not data:
+            return None
+        serial = data.split(",")
+        root = TreeNode(int(serial[0]))
+        queue = deque([root])
+        for left, right in zip(serial[1::2], serial[2::2]):
+            node = queue.popleft()
+            if left:
+                node.left = TreeNode(int(left))
+                queue.append(node.left)
+            if right:
+                node.right = TreeNode(int(right))
+                queue.append(node.right)
+        return root
