e34cacf481db0ec3cca8989827b8ac89e0ae4a7d
William R. M. <101368213+reach-will@users.noreply.github.com>
2026-08-13 09:15:33 -0300

    Add: generate-parentheses - submission-28

---
+++ Data Structures & Algorithms/generate-parentheses/submission-28.py
@@ -0,0 +1,22 @@
+class Solution:
+    def backtrack(self, closed_n: int) -> None:
+        if len(self.path) == 2 * self.n:
+            self.result.append(''.join(['(' if opening else ')' for opening in self.path]))
+            return
+
+        if len(self.path) - closed_n < self.n:
+            self.path.append(True)
+            self.backtrack(closed_n)
+            self.path.pop()
+
+        if len(self.path) > 2 * closed_n:
+            self.path.append(False)
+            self.backtrack(closed_n + 1)
+            self.path.pop()
+
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.n = n
+        self.result = []
+        self.path = []
+        self.backtrack(0)
+        return self.result
