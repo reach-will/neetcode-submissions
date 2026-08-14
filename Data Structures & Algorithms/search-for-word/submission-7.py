6692e68ee9a528ba589ce7dd6bcb7384c7c234b2
William R. M. <101368213+reach-will@users.noreply.github.com>
2026-08-14 02:35:15 -0300

    Add: search-for-word - submission-7

---
+++ Data Structures & Algorithms/search-for-word/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def explore(self, x: int, y: int) -> bool:
+        if (x, y) in self.visited:
+            return False
+        if self.depth == len(self.word):
+            return True
+        if x < 0 or x >= len(self.board) or y < 0 or y >= len(self.board[0]) or self.board[x][y] != self.word[self.depth]:
+            return False
+        self.depth += 1
+        self.visited.add((x, y))
+        for i, j in [(x+1, y), (x, y+1), (x-1, y), (x, y-1)]:
+            if self.explore(i, j):
+                return True
+        self.visited.remove((x, y))
+        self.depth -= 1
+        return False
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        self.depth = 0
+        self.word = word
+        self.board = board
+        self.visited = set()
+        for i, row in enumerate(board):
+            for j, cell in enumerate(row):
+                if self.explore(i, j):
+                    return True
+        return False
