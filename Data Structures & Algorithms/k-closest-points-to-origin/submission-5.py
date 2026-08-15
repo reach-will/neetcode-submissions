a8e6e1539973b8fa5d13254d030ce64000b612e2
William R. M. <101368213+reach-will@users.noreply.github.com>
2026-08-15 06:50:22 -0300

    Add: k-closest-points-to-origin - submission-5

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-5.py
@@ -0,0 +1,6 @@
+class Solution:
+    def euclideanDistanceFromOrigin(self, points: List[int]) -> float:
+        return math.sqrt(points[0] * points[0] + points[1] * points[1])
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return heapq.nsmallest(k, points, key = self.euclideanDistanceFromOrigin)
