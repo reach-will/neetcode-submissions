cf84db820f969008a529abe03f4ed75914074bf2
William R. M. <101368213+reach-will@users.noreply.github.com>
2026-08-15 02:37:36 -0300

    Add: combinations-of-a-phone-number - submission-9

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-9.py
@@ -0,0 +1,34 @@
+class Solution:
+    def backtrack(self, start: int) -> None:
+        if start == self.n:
+            self.result.append("".join(self.path))
+            return
+
+        for ch in self.digit_to_characters[self.digits[start]]:
+            self.path.append(ch)
+            self.backtrack(start + 1)
+            self.path.pop()
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        self.digits = digits
+        self.n = len(digits)
+
+        self.digit_to_characters = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+
+        self.path = []
+        self.result = []
+        self.backtrack(0)
+
+        return self.result
