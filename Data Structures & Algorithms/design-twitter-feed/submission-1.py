41a8c8bfdbecd89b7448355ff0cc1c4a426ebb1d
William R. M. <101368213+reach-will@users.noreply.github.com>
2026-08-24 09:07:06 -0300

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.py
@@ -0,0 +1,39 @@
+class Twitter:
+    MAX_FEED_LENGTH = 10
+
+    def __init__(self):
+        self.subscribed_to = defaultdict(set)
+        self.post_order = 0
+        self.user_posts = defaultdict(list)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.user_posts[userId].append((self.post_order, tweetId))
+
+        if len(self.user_posts[userId]) > self.MAX_FEED_LENGTH:
+            self.user_posts[userId].pop(0)
+
+        self.post_order += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        news_feed = []
+        priority_queue = []
+
+        for followeeId in (self.subscribed_to[userId] | {userId}):
+            for post in reversed(self.user_posts[followeeId]):
+                if len(priority_queue) < self.MAX_FEED_LENGTH:
+                    heapq.heappush(priority_queue, post)
+
+                else:
+                    if priority_queue[0] >= post:
+                        break
+
+                    heapq.heappushpop(priority_queue, post)
+
+        return [tweetId for _, tweetId in sorted(priority_queue, reverse=True)]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.subscribed_to[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.subscribed_to[followerId]:
+            self.subscribed_to[followerId].remove(followeeId)
